d2b706e011b072d9398cf33ecdd3e39b56044467
Hassan-BinMousa <smartblood007@yahoo.com>
2023-10-07 04:52:21 +0300

    add-pj

---
+++ resources/views/adminlte/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @section('title')
+  DashBord
+  @endsection
+@include('dashbord.head')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{asset('assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    
+  </div>
+
+@include('dashbord.main-header')
+
+@include('dashbord.main-sidebar')
+
+@yield('content')
+
+@include('dashbord.footer')
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+@include('dashbord.footer-script')
+</body>
+</html>
